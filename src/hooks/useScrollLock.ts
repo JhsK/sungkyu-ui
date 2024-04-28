0dd312f11a3800fb3210acf3c4b553b89a74c047
JhsK <qwe6293@nate.com>
2024-04-28 16:46:52 +0900

    scroll lock custom hook

---
+++ src/hooks/useScrollLock.ts
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+export default function useScrollLock() {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+}
