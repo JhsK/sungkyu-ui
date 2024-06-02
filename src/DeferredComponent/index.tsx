24310eefa4fc1e0b1bad4dd4301516e5974b0253
JhsK <qwe6293@nate.com>
2024-06-03 00:22:47 +0900

    feat: DeferredCompoent

---
+++ src/DeferredComponent/index.tsx
@@ -0,0 +1,45 @@
+import { ReactNode, useEffect, useState } from "react";
+
+interface IDeferredComponentProps {
+  children: ReactNode;
+  minWidth: string;
+  minHeight: string;
+  //   style?: CSSProp;
+}
+
+function DeferredComponent({
+  children,
+  minWidth,
+  minHeight,
+}: //   css,
+IDeferredComponentProps) {
+  const [isDeferred, setIsDeferred] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsDeferred(true);
+    }, 200);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  if (!isDeferred) {
+    return (
+      <div
+      // minWidth={minWidth}
+      // minHeight={minHeight}
+      // css={css}
+      />
+    );
+  }
+  return <>{children}</>;
+}
+
+// const StyledDeferredComponent = styled.div<
+//   Omit<IDeferredComponentProps, "children" | "css">
+// >`
+//   min-width: ${(props) => props.minWidth};
+//   min-height: ${(props) => props.minHeight};
+// `;
+
+export default DeferredComponent;
